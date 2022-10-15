3011aaebe5f12f957b23f70514c7752c8792d99e
AzhagappanT <113381317+AzhagappanT@users.noreply.github.com>
2022-10-15 20:25:23 +0530

    Add files via upload

---
+++ Personal Expense Tracker/Assessments/M1 Lead (AZHAGAPPAN T)/Assignment 3/storage & watson/app.py
@@ -0,0 +1,16 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+app = Flask(__name__)
+app.secret_key="dfljsgfddgfh"
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/branch/<id>")
+def branch(id):
+    return render_template('branch.html',id =id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080,debug=True)
+    
+    
